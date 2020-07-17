d30a7c2827e1fad06b07984fc70ab0baf0ba6d90
alpha-i5 <ywgyg497@gmail.com>
2020-07-17 11:45:50 +0900

    chat-space9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,19 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
+      
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
@@ -15,5 +25,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email)
-  end
+  end  
 end
